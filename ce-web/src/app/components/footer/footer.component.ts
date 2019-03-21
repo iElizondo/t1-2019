b3c9d3b38a6714e9fa281c0add98eee02d1c0bd8
Isabel Elizondo <isabel.elizondo.meza@est.una.ac.cr>
2019-03-20 20:54:04 -0600

    Face 2
    Paso del 1-3 de Trabajando con el localstorage

--- ce-web/src/app/components/footer/footer.component.ts
+++ ce-web/src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataStorageService } from 'src/app/service/dataStorage/data-storage.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   lastUpdate:Date
-  constructor() {
+  data:string;
+  constructor(private _dataStorage:DataStorageService) {
     this.lastUpdate=new Date();
+    _dataStorage.setObjectValue("prueba","prueba");
+    this.data = _dataStorage.getObjectValue("prueba");
+    console.log(this.data);
    }
 
   ngOnInit() {
